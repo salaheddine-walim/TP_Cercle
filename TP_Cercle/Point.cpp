891ce7a2958c3c17ba0ddc77e6321446cdc8010c
salah <salah@git.com>
2020-11-19 04:56:48 +0100

    Revert "Revert "Ajoutez des fichiers projet.""
    This reverts commit eece04b8d35e57a571ae2e8c4110606c7624861d.

---
+++ TP_Cercle/Point.cpp
@@ -0,0 +1,33 @@
+#include "Point.h"
+
+Point::Point()
+{
+	this->x = 0.0;
+	this->y = 0.0;
+}
+
+Point::Point(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+double Point::getX()
+{
+	return this->x;
+}
+
+double Point::getY()
+{
+	return this->y;
+}
+
+void Point::setX(double x)
+{
+	this->x = x;
+}
+
+void Point::setY(double y)
+{
+	this->y = y;
+}
